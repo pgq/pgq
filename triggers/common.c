17102523f6d350c6b6b3c2b42fc1ff3d90b214af
Peter Eisentraut <peter@eisentraut.org>
2019-08-22 10:18:41 +0200

    Fix for PostgreSQL 12
    PostgreSQL 12 requires even equality functions of a collatable data
    type to be called with a valid collation, otherwise these will error
    out.

--- triggers/common.c
+++ triggers/common.c
@@ -902,7 +902,9 @@ int pgq_is_interesting_change(PgqTriggerEvent *ev, TriggerData *tg)
 			 * attributes and do string comparison.
 			 */
 			if (OidIsValid(opr_oid)) {
-				if (DatumGetBool(FunctionCall2(opr_finfo_p, old_value, new_value)))
+				if (DatumGetBool(FunctionCall2Coll(opr_finfo_p,
+								   TupleDescAttr(tupdesc, i)->attcollation,
+								   old_value, new_value)))
 					continue;
 			} else {
 				char *old_strval = SPI_getvalue(old_row, tupdesc, i + 1);
