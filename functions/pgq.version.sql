43baa25cf1092888924c2d7413432c3bb8085a53
Gavin M. Roy <gavinmroy@gmail.com>
2020-01-10 16:05:51 -0500

    Bump the version to match the current version

--- functions/pgq.version.sql
+++ functions/pgq.version.sql
@@ -7,7 +7,7 @@ returns text as $$
 --      version and only bumped when database code changes.
 -- ----------------------------------------------------------------------
 begin
-    return '3.2.6';
+    return '3.3.1';
 end;
 $$ language plpgsql;
 
