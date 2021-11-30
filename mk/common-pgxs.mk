ae255381926014148bc442a920eeeb09afe0581d
Marko Kreen <markokr@gmail.com>
2021-11-30 11:04:21 +0200

    More deb cleanup

--- mk/common-pgxs.mk
+++ mk/common-pgxs.mk
@@ -148,8 +148,6 @@ checkver:
 		|| { echo "ERROR: $(EXTENSION).control has wrong version"; exit 1; }
 	@test -f "docs/notes/v$(EXT_VERSION).md" \
 		|| { echo "ERROR: notes missing: docs/notes/v$(EXT_VERSION).md"; exit 1; }
-	@head debian/changelog | grep -q "[(]$(EXT_VERSION)-" debian/changelog \
-		|| { echo "ERROR: debian/changelog has wrong version"; exit 1; }
 
 all: checkver
 
